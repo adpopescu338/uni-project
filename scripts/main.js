559c7bd005cdc3775b0d14a3e04636a142e47cd2
Alex Popescu <adpopescu@shipserv.com>
2022-11-28 21:55:39 +0000

    add fetch polyfill

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
+const fetchPolyfill = document.createElement("script");
+fetchPolyfill.src = "https://cdn.jsdelivr.net/npm/fetch-polyfill@0.8.2/fetch.min.js";
+document.querySelector('head').appendChild(fetchPolyfill);
+
 import initHeader, {updateHeader} from "./header.js";
 import initFooter from "./footer.js";
 initHeader();
