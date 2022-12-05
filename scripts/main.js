992e2872dd5ff93643af2d9cd71a022335e7445b
Alex Popescu <adpopescu@shipserv.com>
2022-12-05 07:41:28 +0000

    x

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,17 @@
 const fetchPolyfill = document.createElement("script");
-fetchPolyfill.src = "https://cdn.jsdelivr.net/npm/fetch-polyfill@0.8.2/fetch.min.js";
-document.querySelector('head').appendChild(fetchPolyfill);
+fetchPolyfill.src =
+  "https://cdn.jsdelivr.net/npm/fetch-polyfill@0.8.2/fetch.min.js";
+document.querySelector("head").appendChild(fetchPolyfill);
 
-import initHeader, {updateHeader} from "./header.js";
+import initHeader, { updateHeader } from "./header.js";
 import initFooter from "./footer.js";
 initHeader();
 initFooter();
-getSession().then(session=>{
-  if(session){
+getSession().then((session) => {
+  if (session) {
     updateHeader();
   }
-})
+});
 
 function getSession() {
   return new Promise((resolve) => {
